798ea3fcaec2b181a49feec65101518b3f7c86ad
K Lavanya Reddy <117257210+kambhamlavanya12@users.noreply.github.com>
2022-11-29 15:00:43 +0530

    Add files via upload

---
+++ tuple2.py
@@ -0,0 +1,24 @@
+# x=(1)
+# print(x,type(x))
+
+# x=(1,)
+# print(x,type(x))
+
+# pr=('apple','reliance','asus','dell','samsung')
+# pr[1]="Motorolla"
+
+# n="Hello" ,  "World"
+# print(n,type(n)) 
+# print(n[2])
+
+# v=(1,4.2,'hello',[2,3],('H','D'),None,True)
+# print(v,type(v)) 
+
+v=(1,4.2,'hello',[2,3],('H','D'))
+v[0]=21
+print(v.count(4.2))
+print(v.index([52,7]))
+
+
+
+# print(dir(tuple))
